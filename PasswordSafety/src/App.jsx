f9333dfb0abab932099352e8f07080274f390b5c
catsriel <danbro07@imkatta.no>
2025-05-09 09:44:18 +0200

    Add Header

--- PasswordSafety/src/App.jsx
+++ PasswordSafety/src/App.jsx
@@ -3,12 +3,15 @@ import { useState } from 'react'
 // Components
 import Navbar from './navbar'
 import LinkPath from './links'
+import HeaderText from './header'
 
 function App() {
   return (
     <div>
         <Navbar></Navbar>
         <LinkPath></LinkPath>
+
+        <HeaderText name="Passord Sikkerhet"></HeaderText>
     </div>
   )
 }
---
+++ PasswordSafety/src/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HeaderText(props){
+    return (
+        <h1 className="headerText"> {props.name} </h1>
+    )
+}
+
+export default HeaderText;
