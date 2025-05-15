e2ec3a6a4ea9ad885b3a01873ecd968b40534459
catsriel <danbro07@imkatta.no>
2025-05-15 15:14:07 +0200

    Start implementing the password analysis itself
    i have a love hate relationship with react

--- PasswordSafety/src/PasswordChecker.jsx
+++ PasswordSafety/src/PasswordChecker.jsx
@@ -4,22 +4,38 @@ import Text from "./text";
 import Subheader from "./subheader";
 
 function PasswordCheck(){
+    const [inputfield, setInput] = useState(""); // user input
+    const [special, setSpecial] = useState(0); // special character amount
+    const [numbers, setNumbers] = useState(0); // number amount
+    const [capital, setCapital] = useState(0); // capital letter amount
+    const [lowercase, setLowercase] = useState(0);// lowercase amount
+    const [lengthOf, setLength] = useState(0); // password length
+    const [finalGrade, setGrade] = useState("Farlig Usikkert Passord");
+
     return (
         <section id="passwordCheckSect">
             <Subheader name="Sjekk Passordene Dine"></Subheader>
-            <Text txt="Nettvett har utviklet en passordsjekker, som gir en karakter tilbake basert på hvor sterkt passordet er, og hva som kan gjøres bedre."></Text>
+            <Text txt="Nettvett har utviklet en passordsjekker, som gir en karakter tilbake basert på hvor sterkt passordet er, og hva som kan gjøre passordet sterkere."></Text>
 
             <div id="Checker">
                 
                 <Subheader name="Analyser et Passord" id="subheaderAnalyze"></Subheader>
 
                 <div id="passwordField">
-                    <input type="text" placeholder="Passord"></input>
-                    <p>Farlig Usikkert Passord</p>
+                    <input 
+                    type="text"
+                    placeholder="Passord"
+                    onChange={HandleTyping}></input>
+                    <p>{finalGrade}</p>
                 </div>
                 
                 <div id="outerbox">
                     <div className="leftField box">
+                        <p>Lengde: {lengthOf}</p>
+                        <p>Symboler {special}</p>
+                        <p>Tall: {numbers}</p>
+                        <p>Store Bokstaver: {capital}</p>
+                        <p>Små Bokstaver: {lowercase}</p>
 
                     </div>
 
@@ -30,6 +46,12 @@ function PasswordCheck(){
             </div>
         </section>
     )
+
+    function HandleTyping(e){
+        let tar = e.target
+        setInput(tar.value);
+        setLength(tar.value.length)
+    }
 }
 
 export default PasswordCheck;
