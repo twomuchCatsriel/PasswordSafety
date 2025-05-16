904bc8f2f8f9eb461cb8f7107a89f15b5e8988d8
catsriel <danbro07@imkatta.no>
2025-05-16 14:29:44 +0200

    Add prototype of password scoring system
    Password Scoring System will be redone later on.

--- PasswordSafety/src/PasswordChecker.jsx
+++ PasswordSafety/src/PasswordChecker.jsx
@@ -11,7 +11,30 @@ function PasswordCheck(){
     const [lowercase, setLowercase] = useState(0);// lowercase amount
     const [lengthOf, setLength] = useState(0); // password length
     const [finalGrade, setGrade] = useState("Farlig Usikkert Passord");
-    const SpecialCharacters = "~!@#$%^&*()_+=?/\"'¤`´¨;,:.-<>§|"
+    const SpecialCharacters = "~!@#$%^&*()_+=?/\"'¤`´¨;,:.-<>§|+"
+    const commonPasswords = [
+  "123456", "password", "12345678", "qwerty", "123456789",
+  "12345", "1234", "111111", "1234567", "dragon",
+  "123123", "baseball", "abc123", "football", "monkey",
+  "letmein", "696969", "shadow", "master", "666666",
+  "qwertyuiop", "123321", "mustang", "1234567890", "michael",
+  "654321", "pussy", "superman", "1qaz2wsx", "7777777",
+  "fuckyou", "121212", "000000", "qazwsx", "123qwe",
+  "killer", "trustno1", "jordan", "jennifer", "zxcvbnm",
+  "asdfgh", "hunter", "buster", "soccer", "harley",
+  "batman", "andrew", "tigger", "sunshine", "iloveyou",
+  "fuckme", "2000", "charlie", "robert", "thomas",
+  "hockey", "ranger", "daniel", "starwars", "klaster",
+  "112233", "george", "asshole", "computer", "michelle",
+  "jessica", "pepper", "1111", "zxcvbn", "555555",
+  "11111111", "131313", "freedom", "777777", "pass",
+  "fuck", "maggie", "159753", "aaaaaa", "ginger",
+  "princess", "joshua", "cheese", "amanda", "summer",
+  "love", "ashley", "6969", "nicole", "chelsea",
+  "biteme", "matthew", "access", "yankees", "987654321",
+  "dallas", "austin", "thunder", "taylor", "matrix"
+];
+
 
     return (
         <section id="passwordCheckSect">
@@ -76,6 +99,58 @@ function PasswordCheck(){
             }
 
         }
+
+        // Set the final grade
+        let points = 0;
+
+        if(totalSpec > 0){ // Special Characters
+            points += 1;
+        }
+        if(totalNumbers > 0){  // numbers 
+            points += 1;
+        }
+        if(totalLetters[0] >= 2){ // LowerCase Letters
+            points += 1;
+        }
+        if(totalLetters[1] >= 2){ // UpperCase Letters
+            points += 1;
+        }
+
+        if(tar.length <= 4){ // Less than 4 characters
+            points -= 1;
+        }
+        if(tar.length < 8){ // Less than 8 characters
+            points -= 1;
+        }
+        
+        if(tar.length >= 8){ // 8 or More 
+            points += 1;
+        }
+        if(tar.length >= 14){ // 14 or More
+            points += 1;
+        }
+
+        if(commonPasswords.includes(tar.toLowerCase())){ // If password is among the global top 100 most used passwords
+            points = 0;
+        }
+
+        // Set Grade
+        if(points < 2){
+            setGrade("Farlig")
+        }
+        else if(points <= 4){
+            setGrade("Litt Trygt")
+        }
+        else if(points == 5){
+            setGrade("Trygt")
+        }
+        else if(points == 6){
+            setGrade("Ekstremt Trygt")
+        }
+
+
+
+        
 
         // Update all the states simultaneously
         setNumbers(totalNumbers);
